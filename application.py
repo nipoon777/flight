53a484654aa6fa9d23cc38f748601d6e70bc89bc
nipoon777 <ubuntu@ide.ide.b83c56d1-ae66-4225-83a0-d1c47ba38b9a.svc.cluster.local>
2019-10-05 12:35:30 +0000

    AllThe best

--- application.py
+++ application.py
@@ -1,27 +1,34 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import Session
-from werkzeug.exceptions import default_exceptions
-from cs50 import SQL
+import os
+
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-db = SQL("postgres://iwcuuvojalcppu:300be0bc205b34207dbbd18218b8155c0fa69730eeb0497ef1bca3670a34b834@ec2-54-235-86-101.compute-1.amazonaws.com:5432/d2d3alomvpcnov")
+engine = create_engine(os.getenv("postgres://iwcuuvojalcppu:300be0bc205b34207dbbd18218b8155c0fa69730eeb0497ef1bca3670a34b834@ec2-54-235-86-101.compute-1.amazonaws.com:5432/d2d3alomvpcnov"))
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    flights = db.execute("SELECT * FROM flights")
-    return render_template("index.html",flights = flights)
+    flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("index.html", flights=flights)
 
-@app.route("/book",methods=["POST"])
+@app.route("/book", methods=["POST"])
 def book():
+    """Book a flight."""
 
+    # Get form information.
     name = request.form.get("name")
     try:
         flight_id = int(request.form.get("flight_id"))
     except ValueError:
         return render_template("error.html", message="Invalid flight number.")
 
+    # Make sure the flight exists.
+    if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
+        return render_template("error.html", message="No such flight with that id.")
     db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
-            name = name, flight_id =flight_id )
-
+            {"name": name, "flight_id": flight_id})
+    db.commit()
     return render_template("success.html")
